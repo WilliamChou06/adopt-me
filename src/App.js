167dd9ace94e32acc5b7904001575e66f1059051
William Chou <williamchoudev@gmail.com>
2019-02-11 11:58:02 -0400

    Refactor components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import pf from 'petfinder-client';
 
-const Pet = props => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
-  ]);
-};
+import Pet from './Pet';
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
 
 class App extends React.Component {
-  render(){
-    return React.createElement("div", {}, [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese"
-      }),
-      React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "Cockatiel"
-      }),
-      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-    ]);
+  componentDidMount() {
+    petfinder.breed.list({ animal: `dog` }).then(console.log, console.error)
   }
-};
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Pet name="Luna" animal="dog" breed="Havanese" />
+        <Pet name="Marco" animal="dog" breed="German Sheperd" />
+        <Pet name="Polo" animal="cat" breed="Puss" />
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
-
+ReactDOM.render(React.createElement(App), document.getElementById(`root`));
